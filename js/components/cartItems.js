3b7378b76ed1555aeae3ffb56340110ef5d588d3
Dennis <71315485+dotDennis@users.noreply.github.com>
2021-03-21 19:20:12 +0100

    Bugfixes & Code changes > check desc
    I redid portion of the games list, to use javascript for card generation. But realised I don't have time to fully commit into this idea yet. Need to focus on the other parts first. Made the checkout page more pretty, mainly for mobile, pc is okay.
    
    Added cart +/- reponsive. the total is taken from a list of array, likewise with cart items, so not actively responding to what item u add, though u can just add apex for now (plan was to make an 'api' (just an array), and fetch details form there to the product.html page)
    
    Next up = contact form validation.

---
+++ js/components/cartItems.js
@@ -0,0 +1,5 @@
+const btnPlus = document.querySelector(".btn-plus");
+const btnMinus = document.querySelector(".btn-minus");
+
+btnPlus.addEventListener("click", countPlus);
+btnMinus.addEventListener("click", countMinus);
--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,5 +1,31 @@
-const counter = document.querySelector(".cart-count");
+const cart = document.querySelector(".cart-icon");
+const cartCount = document.querySelector(".cart-count");
 
-if (counter.innerHTML === "0") {
-  counter.style.display = "none";
+const storedCart = localStorage.getItem("cartValue");
+
+let count = 0;
+
+if (storedCart) {
+  count = storedCart;
+  cart.innerHTML = `<span class="cart-count">${count}</span></i>`;
+}
+
+const saveToLocalStorage = () => {
+  localStorage.setItem("cartValue", count);
+};
+
+function countPlus() {
+  count++;
+  saveToLocalStorage();
+  cart.innerHTML = `<span class="cart-count">${count}</span></i>`;
+}
+
+function countMinus() {
+  if (count >= 1) {
+    count--;
+    saveToLocalStorage();
+    cart.innerHTML = `<span class="cart-count">${count}</span></i>`;
+  } else if (count < 1) {
+    cart.innerHTML = ``;
+  }
 }
